07ff4ee4dd05bea5b9e5b96b0b0cb85b11532cef
SZaboAnna21 <113799568+SZaboAnna21@users.noreply.github.com>
2023-02-20 19:08:14 +0100

    imporvement (#1)

--- lastproject get the word.py
+++ lastproject get the word.py
@@ -1,39 +1,55 @@
 from googletrans import Translator
 import time
 import ast
+import os
+
+
 translator = Translator()
 cardsLIST=[]
+cardList2=[]
+print("HELLO")
+validanswer=["igen","nem"]
+validanswertöröl=["töröl", "marad"]
 
-#1234
+
+
+
 with open("pytonUDEMY2023\\flie.txt") as f:
     cardsLIST=f.read()
 
 if not cardsLIST:
     print ("list is empty")
     cardsLIST=[]
-    cardList2=[]
 else:
     [s.strip() for s in cardsLIST[1:-1].split(',')]
     cardsLIST=ast.literal_eval(cardsLIST)
-    cardList2 =cardsLIST
 
 print(f"Egyenlore {len(cardsLIST)} db szópár van a fáljba ")
 if len(cardsLIST) != 0:
-    megnéziaszavakat=input("?eg akarod nézni ezeket a szavakat? (igen/nem)  ")
-    if megnéziaszavakat=="igen":
-        print(cardsLIST)
-        törlés=input("Maradjanak vagy töröljük ezeket a szavakt? (marad/töröl)  ")
-        if törlés =="töröl":
-            cardsLIST=[]
-            cardList2=[]
+    loop=True
+    while loop:
+        megnéziaszavakat=input("meg akarod nézni ezeket a szavakat? (igen/nem)  ")
+        if megnéziaszavakat in validanswer:
+                loop=False
+                if megnéziaszavakat=="igen":
+                    print(cardsLIST)
+                    loop = True
+                    while loop:
+                        törlés=input("maradjanak vagy töröljük ezeket a szavakt? (marad/töröl)  ")
+                        if törlés in validanswertöröl:
+                            loop=False
+                            if törlés =="töröl":
+                                cardsLIST=[]
+                        else:
+                            print(f"bocsi ezt nem ertem valasz valaszt innem:{validanswertöröl}")
+                            time.sleep(1)
         else:
-            cardsLIST=[]
-    else:
-        cardsLIST=[]
+                print("Kerlek igen vagy nem valaszokat hasznald")
+                time.sleep(1)
 
 
-print(type(cardsLIST))
-print(type(cardList2))
+#print(type(cardsLIST))
+#print(type(cardList2))
 
 
 #faljon lévo dolgok törlése
@@ -49,18 +65,26 @@ def translate(wordhu):
 
 
 def addANewCard():
-    global cardsLIST
-    wordhu=input("Kerlek add meg azt a magyar szot amit angolul akarsz megtanulni:  ")
+    global cardList2
+    wordhu=input("kerlek add meg azt a magyar szot amit angolul akarsz megtanulni:  ")
     worden = translate(wordhu)
     time.sleep(1)
     print(worden)
     newCard = (wordhu, worden)
     time.sleep(1)
-    answer = input(f"Akarod ezt a part({newCard}) hozzadni a listahoz? (igen\nem)  ")
-    if answer == "igen":
-        print("Hozzáadás...")
-        time.sleep(1)
-        list.append(cardsLIST,newCard)
+
+    loop=True 
+    while loop:
+        answer = input(f"Akarod ezt a part({newCard}) hozzadni a listahoz? (igen\nem)  ")
+        if answer in validanswer:
+            loop=False
+            if answer == "igen":
+                print("Hozzáadás...")
+                time.sleep(1)
+                list.append(cardList2,newCard)
+        else:
+            print(f"Kerlek valassz valaszt ezek kozul:{validanswer}")
+            time.sleep(1)
 
 
 
@@ -68,32 +92,44 @@ again = True
 while again:
     addANewCard()
     time.sleep(1)
-    another = input("Akarsz e meg egy part hozzadni az eddigiekhez? (igen\nem)  ")
-    if another =="nem":
-        again = False
+    loop=True
+    while loop:
+        another = input("Akarsz e meg egy part hozzadni az eddigiekhez? (igen\nem)  ")
+        if another in validanswer:
+            loop=False
+            if another =="nem":
+                again = False
 
 
 time.sleep(1)
-print(cardsLIST)
-if (not cardsLIST):
-    if cardList2:
+print(cardList2)
+if (not cardList2):
+    if cardsLIST:
         with open("C:\\Users\\szabo\\OneDrive\\Dokumentumok\\pyton\\complete_coding_Udemy\\pytonUDEMY2023\\flie.txt", "a") as f:
-            f.write(str(cardList2))
+            f.write(str(cardsLIST))
             print("End of program")
     else:
         print("End of program")
 else:
-    answer = input("Hozzáakarod adni ezeket a fáljhoz? (igen/ nem)  ")
-    if answer =="igen":
-        if not cardList2:
-            with open("C:\\Users\\szabo\\OneDrive\\Dokumentumok\\pyton\\complete_coding_Udemy\\pytonUDEMY2023\\flie.txt", "a") as f:
-                f.write(str(cardsLIST))
-            print("Hozzáadás...")
-            print("End of program")
+    loop=True
+    while loop:
+        answer = input("Hozzáakarod adni ezeket a fáljhoz? (igen/ nem)  ")
+        if answer in validanswer:
+            loop=False
+            if answer =="igen":
+                if not cardsLIST:
+                    with open("C:\\Users\\szabo\\OneDrive\\Dokumentumok\\pyton\\complete_coding_Udemy\\pytonUDEMY2023\\flie.txt", "a") as f:
+                        f.write(str(cardList2))
+                    print("Hozzáadás...")
+                    print("End of program")
+                else:
+                    cardList2.extend(cardsLIST)
+                    with open("C:\\Users\\szabo\\OneDrive\\Dokumentumok\\pyton\\complete_coding_Udemy\\pytonUDEMY2023\\flie.txt", "a") as f:
+                        f.write(str(cardList2))
+                    print("Hozzáadás...")
+                    print("End of program")
+            else:
+                print("End of program")
         else:
-            cardsLIST.extend(cardList2)
-            with open("C:\\Users\\szabo\\OneDrive\\Dokumentumok\\pyton\\complete_coding_Udemy\\pytonUDEMY2023\\flie.txt", "a") as f:
-                f.write(str(cardsLIST))
-            print("Hozzáadás...")
-            print("End of program")
-
+            print(f"Kerlek valassz valaszt ezek kozul:{validanswer}")
+            time.sleep(1)
